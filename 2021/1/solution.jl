458d55d21e75629cb67eb10ead6a55e1c9071506
Owen Miller <owenchristianmiller@gmail.com>
2021-11-30 21:12:33 -0800

    solutions: day 1 - part 1 & 2

--- 2021/1/solution.jl
+++ 2021/1/solution.jl
@@ -5,11 +5,32 @@ Day: 1
 
 ===#
 
+# Read Input Data
 data = []
-open("1/test_input.txt") do f
+open("1/input.txt") do f
     while !eof(f)
         s = readline(f)
         push!(data, parse(Int32, s))
     end
 end
-println(s)
+
+# Part 1 - count increasing values
+count = 0
+for i in 2:length(data)
+    if data[i] > data[i-1]
+        count += 1
+    end
+end
+
+println("Part 1: " * string(count))
+
+# Part 2 - count increasing sliding 3-window
+count = 0
+for i in 3:length(data)-1
+    if data[i-1] + data[i] + data[i+1] > data[i-2] + data[i-1] + data[i]
+        count += 1
+    end
+end
+
+
+println("Part 2: " * string(count))
